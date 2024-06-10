f2f611e3100c741b491ad71e60d9dfdf99a37e39
A147R <itikela.anirudh147@gmail.com>
2024-06-10 01:19:24 -0500

    react

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+
+function App(){
+    return <div><ListGroup/></div>
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+function Message(){
+    const name = 'Rajender';
+    return <h1>Hello {name}</h1>;
+}
+export default Message;
